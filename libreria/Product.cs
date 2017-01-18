da8afbe3276e43beaeda74dced81b3c64082d53a
Emiliano Conti <econtirodriguez@outlook.com>
2017-01-17 22:51:54 -0300

    First Commit

---
+++ libreria/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace libreria
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Value { get; set; }
+
+        public double ExpectedEarnings { get; set; }
+
+        public override string ToString()
+        {
+            var nl = System.Environment.NewLine;
+            return $"Name: {this.Name}{nl}Description: {this.Description}{nl}Value: {this.Value}{nl}Expected Earnings: {this.ExpectedEarnings}";
+        }
+    }
+}
---
+++ testProvider/Program.fs
@@ -0,0 +1,36 @@
+﻿// Más información acerca de F# en http://fsharp.org
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+module Provider
+
+open FSharp.ExcelProvider
+open System
+open libreria
+
+type ExcelType = ExcelFile<"typeOfExcel.xlsx">
+
+let mapExcelRowToProduct (x:ExcelType.Row) = 
+    let price = x.Price
+    let cost = x.Cost
+    let product = Product()
+    product.Name <- x.``Product Name``
+    product.Description <- x.Description
+    product.ExpectedEarnings <- (price / cost) * 100.0
+    product.Value <- price
+    product
+
+
+let mapExcelToProducts() = 
+    let file = ExcelType("./../../sample.xlsx")
+    let values = 
+        file.Data
+        |> Seq.where (fun (x:ExcelType.Row) -> not(String.IsNullOrEmpty((string)x.``Product Name``)))
+        |> Seq.map mapExcelRowToProduct
+    values
+
+[<EntryPoint>]
+let main argv = 
+    let values = mapExcelToProducts()
+    let result = String.Join(Environment.NewLine, values |> Seq.map (fun x -> x.ToString()))
+    printfn "%s" result
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
